a61eb3c8b51a52916f4dd11b1e394e1e91fa6b2e
Thomas Larsen <thomas.larsen@hotmail.no>
2018-09-25 01:22:44 +0200

    First try

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import serial
 
 
 
-f = open("/home/pi/imulogger/temp.mtb","wb")
+f = open("/home/pi/imulogger/temp2.mtb","wb")
 
 
 ser = serial.Serial('/dev/ttyUSB0',921600,timeout=10)
 
 ser.flush()
 ser.write(b'\xfa\xff\x40\x00\xc1')
+ConfigWritten = False
 for i in range(100):
 	data = ser.read_until(b'\xfa\xff')
 	if data[0] ==  13:
 		f.write(b'\xfa\xff\x0d')
 		f.write(data)
 		print("found config")
-	printstring = " "
-	f.write(data)
-	for parts in data:
-		printstring = printstring + hex(parts) + ' '
-	print(printstring[:30])
+		ConfigWritten = True
+	#printstring = " "
+	if ConfigWritten:
+		f.write(data)
+	#for parts in data:
+	#	printstring = printstring + hex(parts) + ' '
+	#print(printstring[:30])
 ser.close() 
 f.close()
--- mtbreader.py
+++ mtbreader.py
@@ -1,6 +1,6 @@
 f = open("/home/pi/imulogger/temp.mtb","rb")
 hexstring = ""
-for a in f.read():
+for a in f.read(30):
 	hexstring = hexstring + hex(a) + " "
 print(hexstring)
 f.close()
