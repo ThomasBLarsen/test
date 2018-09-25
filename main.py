679f3a1066ef0d0d1ecea47461cb95c210f45223
Thomas Larsen <thomas.larsen@hotmail.no>
2018-09-25 02:30:47 +0200

    First try 2

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ ser = serial.Serial('/dev/ttyUSB0',921600,timeout=10)
 ser.flush()
 ser.write(b'\xfa\xff\x40\x00\xc1')
 ConfigWritten = False
+DataCollect = b''
+
 for i in range(50000):
 	data = ser.read_until(b'\xfa\xff')
 	if data[0] ==  13:
@@ -19,7 +21,10 @@ for i in range(50000):
 		ConfigWritten = True
 	#printstring = " "
 	if ConfigWritten:
-		f.write(data)
+		DataCollect  += data
+		if 100 % i == 0:
+			f.write(DataCollect)
+			DataCollect = b''
 	#for parts in data:
 	#	printstring = printstring + hex(parts) + ' '
 	#print(printstring[:30])
